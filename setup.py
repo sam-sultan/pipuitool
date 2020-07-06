c4a33c799c908dd549b9eeb0dc90071782712a25
Sam Sultan <sam_sultan@alliedtelesis.com>
2020-07-06 16:00:59 -0700

    Changed version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme_file:
 
 setup_args = dict(
     name='pip-gui-tools',
-    version='0.0.2',
+    version='0.0.3',
     description='A tool for python pip package management',
     long_description_content_type="text/markdown",
     long_description=README,
